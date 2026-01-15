16185463f0741e27094cf227f5ad583edb9948cb
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-01-15 16:57:25 +0530

    Time: 11 ms (7.71%), Space: 46.7 MB (13.46%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,21 +1,19 @@
 class Solution {
     public int thirdMax(int[] nums) {
         
-          TreeSet<Integer> store = new TreeSet<>();
+         TreeSet<Integer> store = new TreeSet<>();
 
-          for(int num : nums){
+         for(int num : nums){
 
-               store.add(num);
+              store.add(num);
+         }
+            if(store.size() < 3){
 
-          }
-
-           if(store.size() < 3){
+                  return store.last();
+            }
+              store.pollLast();
+              store.pollLast();
 
               return store.last();
-           }
-               store.pollLast();
-               store.pollLast();
-
-               return store.last();
     }
 }
