6daa90d43e03041776099c991f33239d4b16b8ba
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-01-25 16:31:09 +0530

    Time: 3 ms (79.38%) | Memory: 11.7 MB (74.06%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,17 +6,16 @@ public:
           int index = -1;
           int res = 0;
 
-          vector<int>dict(256 , -1);
+          vector<int>dict(256, -1);
           for(int i = 0; i<n; i++){
 
                if(dict[s[i]] > index){
 
-                       index = dict[s[i]];
+                     index = dict[s[i]];
                }
-                 dict[s[i]] = i; 
-
-                 res = max(res , i-index);
+                  dict[s[i]] = i;
+                  res = max(res , i-index);
           }
-             return res;
+               return res;
     }
 };
