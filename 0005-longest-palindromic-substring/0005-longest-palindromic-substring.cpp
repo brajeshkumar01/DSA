7058ec3e1bcd50a1d5511e298dfe10ab2923980b
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-01 00:09:10 +0530

    Time: 1782 ms (5%), Space: 491.6 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
+    bool ispalindrome(string& s , int left , int right){
 
-     bool ispalindrome(string& s , int left , int right){
+             while(left < right){
 
-               while(left < right){
+                   if(s[left] != s[right]){
 
-                  if(s[left] != s[right]){
-
-                     return false;
-                  }
-                    left++;
-                    right--;
-               }
-
-                 return true;
-        }
-           string longestPalindrome(string s) {
-            
-            
-            int n = s.length();
-             int left = 0, right =n-1;
-
-            string length1 = "";
-
-            for( left = 0; left<n; left++){
-
-                   for(right = left; right<n; right++){
-
-                        if(ispalindrome(s , left , right)){
-
-                                string len = s.substr(left , right-left+1);
-
-                                if(len.length() > length1.length()){
-
-                                      length1 = len;
-                                }
-                        }
+                        return false;
                    }
+                      left++;
+                      right--;
+             }
+                  return true;
+    }
+    string longestPalindrome(string s) {
+        
+           int n = s.length();
+           int left = 0, right = n-1;
+           string maxlen = "";
+
+            for(left = 0; left<n; left++){
+
+                     for(right = left; right<n; right++){
+
+                            if(ispalindrome(s , left , right)){
+
+                                    string len = s.substr(left, right-left+1);
+                                    if(len.length() > maxlen.length()){
+
+                                         maxlen = len;
+                                    }
+                            }
+                     }
             }
-               return length1;
+               return maxlen;
     }
 };
