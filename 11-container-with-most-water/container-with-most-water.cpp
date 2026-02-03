fb358f962fe97708aaaf5ea1c9b653614162c4d7
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-03 19:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (47.57%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,25 +4,24 @@ public:
         
           int n = height.size();
           int maxwater = 0;
-
-          int left = 0 , right = n-1;
+          int left = 0, right = n-1;
 
           while(left < right){
 
-               int currheight = min(height[left], height[right]);
-               int currwidth  = right - left;
-               int currarea = currheight * currwidth;
+              int currheight = min(height[left], height[right]);
+              int currwidth = right - left;
+              int currarea = currheight * currwidth;
 
-               maxwater = max(maxwater  , currarea);
+                maxwater = max(maxwater , currarea);
 
-              if(height[left] < height[right]){
+                if(height[left] < height[right]){
 
-                left++;
-          }else{
+                      left++;
+                }else{
 
-                right--;
+                      right--;
+                }
           }
-      }
-                return maxwater;
+                 return maxwater;
     }
 };
