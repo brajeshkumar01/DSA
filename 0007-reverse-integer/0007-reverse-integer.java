03d725448b53e54ffed9e43ce4a338e83393e29a
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-03 20:12:39 +0530

    Time: 1 ms (99.72%), Space: 42.5 MB (76.52%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -6,13 +6,12 @@ class Solution {
         while(x != 0){
 
               ans = ans * 10 + x % 10;
-
               x /= 10;
         }
-             if(ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE){
+           if(ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE){
 
-                   return 0;
-             }
-                   return (int) ans;
+                 return 0;
+           }
+              return (int) ans;
     }
 }
