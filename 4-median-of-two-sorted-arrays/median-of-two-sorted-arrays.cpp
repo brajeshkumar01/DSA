79a20f064e6955bd80a7be44ad6f3709174e6860
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-04 15:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 96 MB (18.36%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,50 +1,41 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-         int n1 = nums1.size();
-         int n2 = nums2.size();
-         int i = 0, j = 0;
+         
+          int n1 = nums1.size();
+          int n2 = nums2.size();
+          int i = 0, j = 0;
+          vector<int>merged;
+          while(i < n1 && j < n2){
 
-         vector<int>merged;
-         while(i<n1 && j<n2){
-
-                if(nums1[i] < nums2[j]){
+                 if(nums1[i] < nums2[j]){
 
                       merged.push_back(nums1[i]);
                       i++;
-                }else{
+                 }else{
 
-                     merged.push_back(nums2[j]);
-                     j++;
-                }
-         }
+                       merged.push_back(nums2[j]);
+                       j++;
+                 }
+          }
+            while(i < n1){
 
-           while(i < n1){
+                  merged.push_back(nums1[i]);
+                  i++;
+            }
+              while(j < n2){
 
-                 merged.push_back(nums1[i]);
-                 i++;
-           }
+                   merged.push_back(nums2[j]);
+                   j++;
+              }
+                 int n = n1+n2;
 
-           while(i < n1){
+                 if(n % 2 == 0){
 
-                merged.push_back(nums1[i]);
-                i++;
-           }
+                       return (merged[n/2-1]+merged[n/2])/2.0;
+                 }else{
 
-           while(j < n2){
-
-                merged.push_back(nums2[j]);
-                j++;
-           }
-             int n = n1+n2;
-
-             if(n % 2 == 0){
-
-                  return (merged[n/2-1]+merged[n/2])/2.0;
-             }else{
-
-                  return merged[n/2];
-             }
+                      return(merged[n/2]);
+                 }
     }
 };
