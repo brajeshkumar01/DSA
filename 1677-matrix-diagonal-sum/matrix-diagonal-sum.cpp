9487e6d76a508e44959c74ed667a402a136e9c42
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-06 23:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (53.27%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        
+         
           int m = mat.size();
           int n = mat[0].size();
-
-          int leftsum = 0  , rightsum = 0;
           
-          for(int i= 0; i<m; i++){
+          int leftsum = 0, rightsum = 0;
 
-                 leftsum +=mat[i][i];
-                 rightsum +=mat[i][n-i-1];
+          for(int i = 0; i<m; i++){
+
+                 leftsum+=mat[i][i];
+                 rightsum+=mat[i][n-i-1];
           }
              if(m % 2 == 1){
 
-                  leftsum-=mat[n/2][m/2];  // mid element 2bar repeat hua hai isliye isko remove kar diye hai
+                   leftsum-=mat[m/2][n/2];
              }
-                return leftsum + rightsum;
+                 return leftsum + rightsum;
     }
 };
