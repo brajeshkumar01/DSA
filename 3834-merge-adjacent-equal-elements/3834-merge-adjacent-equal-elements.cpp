478c21dedf4d5ff560d02c53ddf5ba99cab1060d
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-10 00:28:56 +0530

    Time: 8 ms (100%), Space: 190.9 MB (100%) - LeetHub

--- 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     vector<long long> mergeAdjacent(vector<int>& nums) {
           
+              vector<long long>st;
+              st.reserve(nums.size());
 
-            vector<long long>st;
-            st.reserve(nums.size()); // used to preallocate memory
+               for(long long num : nums){
 
-            for(long long num : nums){
+                       while(!st.empty() && st.back() == num){
 
-                 while(!st.empty() && st.back() == num){
+                            num+=st.back();
+                            st.pop_back();
+                       }  
 
-                       num+=st.back();
-                       st.pop_back();
-                 }
-                   st.push_back(num); // yadi mera while walla condition sahi nahi hua then do this
-            }
-               return st;
+                          st.push_back(num);
+               }    
+
+                  return st;
     }
 };
