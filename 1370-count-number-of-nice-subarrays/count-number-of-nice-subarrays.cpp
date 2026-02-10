2199b35c3c6ad16342193202a0028d4e9423bef8
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-10 19:47:44 +0530

    Time: 8 ms (60.66%) | Memory: 71.3 MB (99.77%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-     int kmostodd(vector<int>& nums, int k){
+   
+        int kmostodd(vector<int>& nums , int k){
+           int left = 0, ans = 0 , oddcount = 0;
+           int n = nums.size();
 
-           int left = 0, oddcount = 0, ans = 0;
-            int n = nums.size();
-           for(int right = 0; right<n; right++){
+            for(int right = 0; right < n; right++){
 
-                  if(nums[right] % 2 == 1) oddcount++;
+                      if(nums[right] % 2 == 1) oddcount++;
+                      
+                      while(oddcount > k){
 
-                  while(oddcount > k){
+                              if(nums[left] % 2 == 1)
 
-                         if(nums[left] % 2 == 1) oddcount--;
-                         left++;
-                  }
-                     ans += (right-left+1);
-           }
-               return ans;
-     }
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        
-         return kmostodd(nums , k) - kmostodd(nums , k-1);
+                                    oddcount--;
+                                    left++;
+                      }
+                         ans+=(right-left+1);
+            }
+               return ans; 
     }
+         int numberOfSubarrays(vector<int>& nums, int k) {
+
+                return kmostodd(nums , k) - kmostodd(nums, k-1);
+         }
 };
