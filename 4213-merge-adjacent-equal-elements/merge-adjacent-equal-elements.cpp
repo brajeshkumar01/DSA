dd2fcf223ab7dd3f969b7ae02c753640684cdb1c
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-10 10:08:34 +0530

    Time: 7 ms (94.46%) | Memory: 190.8 MB (99.52%) - LeetSync

--- 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.cpp
+++ 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     vector<long long> mergeAdjacent(vector<int>& nums) {
           
+              vector<long long>st;
+              st.reserve(nums.size());
+             
+             for(long long num : nums){
 
-            vector<long long>st;
-            st.reserve(nums.size()); // used to preallocate memory
+                  while(!st.empty() && st.back() == num){
 
-            for(long long num : nums){
-
-                 while(!st.empty() && st.back() == num){
-
-                       num+=st.back();
-                       st.pop_back();
-                 }
-                   st.push_back(num); // yadi mera while walla condition sahi nahi hua then do this
-            }
-               return st;
+                          num+=st.back();
+                          st.pop_back();
+                  }
+                     st.push_back(num);
+             }
+                return st;
     }
 };
