8273d94c5520194e9f4fc4b193f3cb748fc7bee3
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-10 10:08:32 +0530

    Time: 7 ms (94.46%), Space: 190.8 MB (99.52%) - LeetHub

--- 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
@@ -4,18 +4,16 @@ public:
           
               vector<long long>st;
               st.reserve(nums.size());
+             
+             for(long long num : nums){
 
-               for(long long num : nums){
+                  while(!st.empty() && st.back() == num){
 
-                       while(!st.empty() && st.back() == num){
-
-                            num+=st.back();
-                            st.pop_back();
-                       }  
-
-                          st.push_back(num);
-               }    
-
-                  return st;
+                          num+=st.back();
+                          st.pop_back();
+                  }
+                     st.push_back(num);
+             }
+                return st;
     }
 };
