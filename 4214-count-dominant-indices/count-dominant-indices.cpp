dc718c4f0a2340116bb87cdbc443bd0f3f9ce476
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-10 09:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (95.74%) - LeetSync

--- 4214-count-dominant-indices/count-dominant-indices.cpp
+++ 4214-count-dominant-indices/count-dominant-indices.cpp
@@ -3,17 +3,19 @@ public:
     int dominantIndices(vector<int>& nums) {
          
             int n = nums.size();
-            int count = 0;
             int sum = 0;
+            int count = 0;
 
             for(int i = n-1; i>0; i--){
 
-                sum+= nums[i];
-                if(nums[i-1] > sum / (n - i)){
+                   sum+= nums[i];
 
-                    count++;
-                }
+                   if(nums[i-1] > sum / (n-i)){
+
+                       count++;
+                   }
             }
-                 return count;
+
+               return count;
        }
 };
