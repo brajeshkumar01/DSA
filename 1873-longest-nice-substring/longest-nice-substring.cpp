1f222c8f28d1e69799b19a1f765749ef30f06cda
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-13 19:06:13 +0530

    Time: 8 ms (38.97%) | Memory: 14.2 MB (44.12%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        
+          int n = s.length();
+          if(s.size() < 2) return "";
+          unordered_set<char>uset;
+
+          for(int i = 0; i<n; i++){
+
+               uset.insert(s[i]);
+          }
+            for(int i = 0; i<n; i++){
+
+                 if(uset.count(tolower(s[i])) == true && uset.count(toupper(s[i]))== true)  continue;
+
+                 string prev = longestNiceSubstring(s.substr(0 , i));
+                 string next = longestNiceSubstring(s.substr(i+1));
+
+                 return prev.size() >= next.size() ? prev : next;
+            }
+              return s;
+    }
+};
