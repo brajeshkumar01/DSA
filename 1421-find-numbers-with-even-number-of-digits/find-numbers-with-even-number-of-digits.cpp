feb073af9c5932ddaa7c5fda24e532b7e831621c
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-14 09:40:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (37.09%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         
-         int count = 0;
-         for(int num : nums){
+            int count = 0;
+            for(int num : nums){
 
-              int digitcount = 0;
+                 int digitcount = 0;
 
-              if(num == 0){
+                 if(num == 0){
 
-                    digitcount = 1;
-              }else{
+                       digitcount = 1;
+                 }else{
 
-                    while(num != 0){
+                      while( num != 0){
 
-                         num /= 10;
-                         digitcount++;
-                    }
-              }
-                      if(digitcount % 2 == 0){
-
-                          count++;
+                              
+                          num /= 10;
+                          digitcount++;
                       }
-         }
-                 return count;
-    }
+                 }
+                     if(digitcount % 2 == 0){
+
+                        count++;
+                     }
+            }
+                      return count;
+    } 
 };
