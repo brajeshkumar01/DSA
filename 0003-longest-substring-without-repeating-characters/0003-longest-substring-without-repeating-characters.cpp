7250026d87d5f0228a4c7f1c98d2e672f362f500
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-14 10:13:36 +0530

    Time: 0 ms (100%), Space: 11.7 MB (74.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         
-          int n = s.size();
-          int index = -1;
-          int res = 0;
+         int n = s.size();
+         int res = 0;
+         int index = -1;
+         vector<int>dict(256,-1);
 
-          vector<int>dict(256 , -1);
           for(int i = 0; i<n; i++){
 
-              if(dict[s[i]] > index){
+               if(dict[s[i]] > index){
 
-                 index = dict[s[i]];
-              }
+                    index = dict[s[i]];
+                    
+               }
                  dict[s[i]] = i;
-                 res = max(res , i-index);
+                res = max(res , i-index);
           }
              return res;
     }
