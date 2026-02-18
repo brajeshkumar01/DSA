3b2dc92c693dff3cd74893f3aeb2c14d7bf2f243
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-19 00:36:41 +0530

    Time: 16 ms (20.01%) | Memory: 13.9 MB (63.82%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -3,37 +3,38 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         
          int n = nums.size();
-         sort(nums.begin(), nums.end());
          int result = nums[0]+nums[1]+nums[2];
+         sort(nums.begin(), nums.end());
          int mindiff = INT_MAX;
-         for(int i = 0; i<n-2; i++){
+
+         for(int i  = 0; i<n-2; i++){
 
                 int left = i+1 , right = n-1;
 
                 while(left < right){
 
-                      int sum = nums[i]+nums[left]+nums[right];
+                    int sum = nums[i]+nums[left]+nums[right];
 
-                      if(sum == target){
+                    if(sum == target){
 
-                           return target;
-                      }
-                         if(sum < target){
+                         return target;
+                    }
+                            if(sum < target){
 
-                               left++;
-                              
-                         }else{
-                              right--;
-                                     }
-                                          int mintodiff = abs(sum - target);
-                                     if(mintodiff < mindiff){
+                                  left++;
+                            }else{
 
-                                           mindiff = mintodiff;
-                                           result = sum;
-                                     }
-                               }
-                         }
-                    return result;
-         
+                                right--;
+                            }
+                                  int mintodiff = abs(sum - target);
+
+                                  if(mintodiff < mindiff){
+
+                                      mindiff = mintodiff;
+                                      result = sum;
+                                  }
+                }
+         }
+                  return result;
     }
 };
