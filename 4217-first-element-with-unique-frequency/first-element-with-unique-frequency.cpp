7b56f0e14c8690cd23983c165b88ed4597ee1af9
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-19 19:34:04 +0530

    Time: 183 ms (44.96%) | Memory: 289.6 MB (66.12%) - LeetSync

--- 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int firstUniqueFreq(vector<int>& nums) {
         
-         int n = nums.size();
-         unordered_map<int ,int>freq;
-         for(int i = 0; i<n; i++){
 
-               ++freq[nums[i]];
-         }
-           unordered_map<int ,int>freqmap;
+        unordered_map<int, int>freq;
+        int n = nums.size();
+        for(int i  = 0; i<n; i++){
+
+              ++freq[nums[i]];
+        }
+           unordered_map<int ,int>freqcount;
+
            for(const auto& pair : freq){
 
-                 freqmap[pair.second]++;
+                   freqcount[pair.second]++;
            }
-
               for(int num : nums){
 
-                    if(freqmap[freq[num]]==1){
+                  if(freqcount[freq[num]] == 1){
 
-                          return num;
-                    }
+                        return num;
+                  }
               }
-                return -1;
+                 return -1;
     }
 };
