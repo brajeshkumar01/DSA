32f04ae1a4f27050a341b0d30482c83682ffb88a
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-20 10:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.81%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -8,36 +8,37 @@ public:
           vector<bool>rowzero(m , false);
           vector<bool>colzero(n , false);
 
-          for(int i = 0; i<m; i++){
+            for(int i = 0; i<m; i++){
 
-                for(int j = 0; j<n; j++){
+                  for(int j = 0; j<n; j++){
 
-                       if(matrix[i][j] == 0){
+                        if(matrix[i][j] == 0){
 
-                            rowzero[i] = true;
-                            colzero[j] = true;
-                       }
-                }
-          }
-             for(int i = 0; i<m; i++){
+                               rowzero[i] = true;
+                               colzero[j] = true;
+                        }
+                  }
+            }
+                for(int i = 0; i<m; i++){
 
-                   if(rowzero[i]){
+                      if(rowzero[i]){
 
-                       for(int j = 0; j<n; j++){
+                            for(int j = 0; j<n; j++){
 
-                             matrix[i][j]  = 0;
-                       }
-                   }
-             }
-                for(int j = 0; j<n; j++){
-
-                      if(colzero[j]){
-
-                          for(int i = 0; i<m; i++){
-
-                                matrix[i][j] = 0;
-                          }
+                                  matrix[i][j] = 0;
+                            }
                       }
                 }
+
+                  for(int j = 0; j<n; j++){
+
+                         if(colzero[j]){
+
+                              for(int i = 0; i<m; i++){
+
+                                    matrix[i][j] = 0;
+                              }
+                         }
+                  }
     }
 };
