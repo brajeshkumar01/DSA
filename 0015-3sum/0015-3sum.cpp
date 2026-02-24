aafc6c67c8f52adace2422d099fc9b2ecb6037a5
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-02-25 01:02:39 +0530

    Time: 1263 ms (6.05%), Space: 291.8 MB (8.02%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,27 +6,28 @@ public:
          sort(nums.begin(), nums.end());
          vector<vector<int>>result;
          set<vector<int>>st;
+
          for(int i = 0; i<n-2; i++){
 
-             int left = i+1 , right = n-1;
+               int left = i+1 , right = n-1;
 
-             while(left < right){
+               while(left < right){
 
-                    int sum = nums[i]+nums[left]+nums[right];
+                     int sum = nums[i]+nums[left]+nums[right];
 
-                    if(sum == 0){
+                     if(sum == 0){
 
-                         st.insert({nums[i], nums[left] , nums[right]});
-                         left++;
-                         right--;
-                    }else if(sum < 0){
-
+                          st.insert({nums[i], nums[left], nums[right]});
                           left++;
-                    }else{
+                          right--;
+                     }else if(sum < 0){
 
-                         right--;
-                    }
-              }
+                           left++;
+                     }else{
+
+                          right--;
+                     }
+               }
          }
             result.assign(st.begin() , st.end());
             return result;
