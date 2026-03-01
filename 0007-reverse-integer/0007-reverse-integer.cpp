0a1d7990b9774b89280708e78d21098c82e39985
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-01 22:54:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.16%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,9 +5,9 @@ public:
            long long ans = 0;
            while(x != 0){
 
-                ans = ans * 10 + x % 10;
-                x /=10;
+                 ans = ans * 10 + x % 10;
+                 x /= 10;
            }
-              return (ans < INT_MIN  || ans > INT_MAX)? 0 : ans;
+             return (ans < INT_MIN || ans > INT_MAX) ? 0 : ans;
     }
 };
