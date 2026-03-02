2a0b17ff204c4128760e925298609c659a827afe
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-02 22:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (53.99%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,9 +5,10 @@ public:
            long long ans = 0;
            while(x != 0){
 
-                 ans = ans * 10 + x % 10;
-                 x /= 10;
+                ans = ans * 10 + x % 10;
+                x /= 10;
            }
+
              return (ans < INT_MIN || ans > INT_MAX) ? 0 : ans;
     }
 };
