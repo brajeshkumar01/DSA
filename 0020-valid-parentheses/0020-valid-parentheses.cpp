f3972a9d128d715b6dc80d25b8b0ad9142dc0512
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-03 18:04:11 +0530

    Time: 3 ms (9.59%), Space: 10.1 MB (5.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+          stack<int>stack;
+          for(char ch : s){
+
+              if(ch == '(' || ch == '[' || ch == '{'){
+
+                   stack.push(ch);
+              }else{
+
+                  if(stack.empty()){
+
+                      return false;
+                  }
+
+                    char top = stack.top();
+                    stack.pop();
+
+                    if((ch == ')' && top != '(')||
+                       (ch == '}' && top != '{')||
+                       (ch == ']' && top != '[')){
+
+                             return false;
+                       }
+                }
+          }
+
+            return stack.empty();
+    }
+};
