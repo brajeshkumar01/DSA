9cd6c50986a8c08e5b2082ce702d4e7dc4a32e3f
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-07 10:54:42 +0530

    Time: 0 ms (100%), Space: 11 MB (8.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -5,15 +5,16 @@ public:
         string str = to_string(x);
         int n = str.size();
         int left = 0, right = n-1;
+
         while(left < right){
 
-                if(str[left] != str[right]){
+              if(str[left] != str[right]){
 
-                        return false;
-                }
-                   left++;
-                   right--;
+                  return false;
+              }
+                left++;
+                right--;
         }
-           return true;
+          return true;
     }
 };
