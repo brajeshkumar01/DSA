cccbf6754d3e990e5407698640a150bc5ee602f2
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-14 07:17:14 +0530

    Time: 19 ms (32.02%), Space: 25.5 MB (22.02%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    vector<string>st;
+    void solved(int n , string s){
+
+          if(s.size() == n){
+
+                 st.push_back(s);
+                 return;
+          }
+            for(char c : {'a', 'b' , 'c'}){
+
+                  if(s.empty() || s.back() != c)
+                   
+                      solved(n , s+c);
+            }
+    }
+    string getHappyString(int n, int k) {
+        
+           solved(n , "");
+
+           return k > st.size() ? "" : st[k-1];
+    }
+};
