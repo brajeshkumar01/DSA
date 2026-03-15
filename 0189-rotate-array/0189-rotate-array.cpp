28f11d96e2f5743697241904420ca360c101fcec
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-15 06:10:46 +0530

    Time: 0 ms (100%), Space: 30.4 MB (19.52%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,15 +4,16 @@ public:
         
          int n = nums.size();
          vector<int>temp(n);
+
          k = k % n;
 
-         for(int i = 0; i<n; i++){
+         for(int i =0; i<n; i++){
 
-                temp[(i+k) % n] = nums[i];
-         } 
-             for(int i = 0; i<n; i++){
+              temp[(i+k) % n] = nums[i];
+         }
+            for(int i = 0; i<n; i++){
 
-                    nums[i] = temp[i];
-             }
+                  nums[i] = temp[i];
+            }
     }
 };
