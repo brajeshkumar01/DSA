cc11d30197d4d330e5a351002bb761e6a833146a
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-15 14:17:25 +0530

    Time: 74 ms (18.97%), Space: 52.1 MB (5.06%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+       int n = arr.size();
+       if(n == 0) return {};
+
+       set<int>uniqueele(arr.begin() , arr.end());
+       unordered_map<int , int> ranks;
+       int rank = 1;
+
+       for(const int &ele : uniqueele)
+
+              ranks[ele] = rank++;
+
+              vector<int>ans;
+              for(int &ele : arr)
+
+                   ans.push_back(ranks[ele]);
+       
+          return ans;
+    }
+};
