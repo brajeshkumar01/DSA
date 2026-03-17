368e270e6b86e4f5693cf6120cc39fef142e0b0e
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-17 17:42:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -7,18 +7,17 @@ public:
           vector<int>result;
 
           result.push_back(nums[0]);
-
           for(int i = 0; i<n; i++){
 
                 if(nums[i] > result.back()){
 
-                      result.push_back(nums[i]);
-                      len++;
+                        result.push_back(nums[i]);
+                        len++;
                 }
 
-                auto index = lower_bound(result.begin() , result.end(), nums[i]);
-                *index = nums[i];
+                 auto index = lower_bound(result.begin(), result.end(), nums[i]);
+                 *index = nums[i];
           }
-             return len;
+            return len;
     }
 };
