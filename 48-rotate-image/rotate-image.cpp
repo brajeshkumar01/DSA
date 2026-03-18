fbcc34f3ecd19ff7c124f2040cb7e60dc2083004
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-18 07:51:24 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (92.57%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -5,23 +5,23 @@ public:
          int m = matrix.size();
          int n = matrix[0].size();
 
-         for(int i = 0; i<m; i++){
+           for(int i = 0; i<m; i++){
 
-              for(int j = i; j<n; j++){
+                  for(int j = i; j<n; j++){
 
-                    int temp = matrix[i][j];
-                    matrix[i][j] = matrix[j][i];
-                    matrix[j][i] = temp;
-              }
-         }
-             for(int i = 0; i<m; i++){
+                      int temp = matrix[i][j];
+                      matrix[i][j] = matrix[j][i];
+                      matrix[j][i] = temp;
+                  }
+           }
+              for(int i = 0; i<m; i++){
 
                   for(int j = 0; j<n/2; j++){
 
-                      int temp = matrix[i][j];
-                      matrix[i][j] = matrix[i][n-j-1];
-                      matrix[i][n-j-1] = temp;
+                       int temp = matrix[i][j];
+                       matrix[i][j] = matrix[i][n-j-1];
+                       matrix[i][n-j-1] = temp;
                   }
-             }
+              }
     }
 };
