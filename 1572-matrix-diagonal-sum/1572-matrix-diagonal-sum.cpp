7446bd72f6658e7caec62c3b723b6756d9add106
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-18 16:19:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -9,13 +9,13 @@ public:
 
          for(int i = 0; i<m; i++){
 
-                   leftsum += mat[i][i];
-                   rightsum += mat[i][n-i-1];        
-         } 
-             if(m % 2 == 1){
+              leftsum += mat[i][i];
+              rightsum += mat[i][n-i-1];
+         }
+            if(m % 2 == 1){
 
-                   leftsum -= mat[m/2][n/2];
-             }
+                  leftsum -= mat[m/2][n/2];
+            }
                return leftsum + rightsum;
     }
 };
