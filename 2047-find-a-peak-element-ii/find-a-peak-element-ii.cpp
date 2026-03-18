a0d6eb355fcdc3f81de7cf89407ceba4c767e05b
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-18 17:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (92.95%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         
-          int m = mat.size();
-          int n = mat[0].size();
+        int m = mat.size();
+        int n = mat[0].size();
 
-          int maxval = mat[0][0];
-          int rowval = 0 , colval = 0;
+        int maxval = mat[0][0];
+        int rowval = 0, colval = 0;
 
           for(int i = 0; i<m; i++){
 
-              for(int j = 0; j<n; j++){
+               for(int j = 0; j<n; j++){
 
                      if(mat[i][j] > maxval){
 
-                          maxval = mat[i][j];
-                          rowval = i;
-                          colval = j;
-                     }
-              }
-          }
+                            maxval = mat[i][j];
 
+                            rowval = i;
+                            colval = j;
+                     }
+               }
+          }
              return {rowval , colval};
     }
 };
