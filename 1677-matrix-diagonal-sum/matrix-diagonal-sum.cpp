e4ba9f749a0063c17e17d6db567bca74de3d801b
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-20 00:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (15.07%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -4,18 +4,17 @@ public:
          
          int m = mat.size();
          int n = mat[0].size();
-
-         int leftsum = 0, rightsum = 0;
-
+         int leftsum = 0 , rightsum = 0;
+         
          for(int i = 0; i<m; i++){
 
-              leftsum += mat[i][i];
-              rightsum += mat[i][n-i-1];
+               leftsum += mat[i][i];
+               rightsum += mat[i][n-i-1];
          }
             if(m % 2 == 1){
 
-                  leftsum -= mat[m/2][n/2];
+                   leftsum -= mat[m/2][n/2];
             }
-               return leftsum + rightsum;
+              return leftsum + rightsum;
     }
 };
