c2f796b186fd97651424351d3d779e2b615e5241
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-21 11:10:47 +0530

    Time: 1842 ms (5.01%) | Memory: 491.8 MB (5.04%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    bool ispalindrome(string& s , int left , int right){
+    
+    bool isvalid(string& s , int left , int right){
 
-             while(left < right){
+            while(left < right){
 
-                   if(s[left] != s[right]){
+                  if(s[left] != s[right]){
 
-                        return false;
-                   }
-                      left++;
-                      right--;
-             }
-                  return true;
+                       return false;
+                  }
+                    left++;
+                    right--;
+            }
+              return true;
     }
     string longestPalindrome(string s) {
         
            int n = s.length();
-           int left = 0, right = n-1;
            string maxlen = "";
+           int left = 0, right = n-1;
 
-            for(left = 0; left<n; left++){
+           for(left = 0; left <n; left++){
 
-                     for(right = left; right<n; right++){
+                 for(right = left; right < n; right++){
 
-                            if(ispalindrome(s , left , right)){
+                       if(isvalid(s , left , right)){
 
-                                    string len = s.substr(left, right-left+1);
-                                    if(len.length() > maxlen.length()){
+                             string len = s.substr(left , right-left+1);
 
-                                         maxlen = len;
-                                    }
-                            }
-                     }
-            }
-               return maxlen;
+                             if(len.length() > maxlen.length()){
+
+                                   maxlen = len;
+                             }
+                       }
+                 }
+           }
+              return maxlen;
     }
 };
