df6b956e930bbf4a5c15ed5ad0e0886b4a3b4a26
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-22 10:20:57 +0530

    Time: 3 ms (24.86%), Space: 23 MB (30.6%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,28 +3,50 @@ public:
     int candy(vector<int>& ratings) {
         
         int n = ratings.size();
-        vector<int>toffees(n, 1);
-// left to right
+        vector<int>toffees(n , 1);
+
+         // left to right
+
          for(int i = 1; i<n; i++){
 
                if(ratings[i] > ratings[i-1]){
 
-                    toffees[i] = toffees[i-1]+1;
+                     toffees[i] = toffees[i-1]+1;    
                }
          }
-            for(int i = n-2; i>=0; i--){
 
-                   if(ratings[i] > ratings[i+1]){
+         // right to left
 
-                        toffees[i] = max(toffees[i], toffees[i+1]+1);
-                   }
-            }
+            for(int i = n-2; i >= 0; i--){
+
+                 if(ratings[i] > ratings[i+1]){
+
+                     toffees[i] = max(toffees[i], toffees[i+1]+1);
+                 }
+            } 
                int maxtoffees = 0;
 
                for(int i = 0; i<n; i++){
 
-                     maxtoffees += toffees[i];
+                    maxtoffees += toffees[i];
                }
-                  return maxtoffees;
+                  
+                   return maxtoffees;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
