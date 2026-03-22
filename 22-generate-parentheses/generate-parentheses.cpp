636f4194e16361778f67075244ded1fa54e1c476
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-22 19:59:42 +0530

    Time: 40 ms (5.05%) | Memory: 46.4 MB (5.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-     vector<string>result;
+    
+    vector<string>result;
 
-     bool isvalid(string s){
+    bool isvalid(string s){
 
            int count = 0;
            for(char ch : s){
 
-                if(ch == '('){
+                 if(ch == '('){
 
-                      count++;
-                }else{
+                       count++;
+                 }else{
+                     
+                       count--;
+                 }if(count < 0){
 
-                      count--;
-                }if(count < 0){
-
-                     return false;
-                }
+                      return false;
+                 }
            }
-              return count == 0;
-     }
+               return count == 0;
+    }
 
-         void solved(string &curr , int n){
+       void solved(string &curr , int n){
 
-                if(curr.length() == n * 2){
+            if(curr.length() == n * 2){
 
-                       if(isvalid(curr))
+                  if(isvalid(curr))
+                  result.push_back(curr);
+                  return;
+            }
+              curr.push_back('(');
+              solved(curr , n);
+              curr.pop_back();
 
-                       result.push_back(curr);
-                       return;
-                }
-
-                  curr.push_back('(');
-                  solved(curr, n);
-                  curr.pop_back();
-
-                  curr.push_back(')');
-                  solved(curr, n);
-                  curr.pop_back();
-         }
+              curr.push_back(')');
+              solved(curr , n);
+              curr.pop_back();
+       }
     vector<string> generateParenthesis(int n) {
         
-           string curr = "";
-           solved(curr , n);
-           return result;
+        string curr = "";
+        solved(curr , n);
+        return result;
     }
 };
