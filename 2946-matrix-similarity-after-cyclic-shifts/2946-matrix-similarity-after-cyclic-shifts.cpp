d8c461aba0dec7d332415f1d89f98bbdb8125b57
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-27 09:17:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<vector<int>> result(m, vector<int>(n));
 
-        // Compute the rotated version of the matrix
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                result[i][(j + k) % n] = mat[i][j];
-            }
-        }
+           int m = mat.size();
+           int n = mat[0].size();
 
-        // Check if the original matrix and the rotated matrix are similar
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(mat[i][j] != result[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
+           vector<vector<int>>result(m, vector<int>(n));
+
+           for(int i = 0; i<m; i++){
+
+               for(int j = 0; j<n; j++){
+
+                     result[i][(j+k) % n] = mat[i][j];
+               }
+           }
+              for(int i = 0; i<m; i++){
+
+                  for(int j = 0; j<n; j++){
+
+                         if(mat[i][j] != result[i][j]){
+
+                              return false;
+                         }
+                  }
+              }
+                 return true;
     }
 };
