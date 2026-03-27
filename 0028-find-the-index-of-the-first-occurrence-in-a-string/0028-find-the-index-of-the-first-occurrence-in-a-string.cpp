fc142d61682068820f121b79a6c565cbbc560b43
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-27 15:37:50 +0530

    Time: 23 ms (14.52%), Space: 72.9 MB (5.47%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,13 +5,13 @@ public:
        int n1 = haystack.size();
        int n2 = needle.size();
 
-        for(int i = 0; i<n1; i++){
+       for(int i = 0; i<n1; i++){
 
-              if(needle == haystack.substr(i , n2)){
+             if(needle == haystack.substr(i  , n2)){
 
-                    return i;
-              }
-        }
-                 return -1;
+                   return i;
+             }
+       }
+          return -1;
     }
 };
