fedbda000cef1a4d74696b367db34124d55de08a
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-29 14:33:42 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.33%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        
+          if(s.length() != goal.length()) return false;
+
+          if(s.empty() && goal.empty()) return true;
+
+          string temp = s + s;
+
+          return temp.find(goal) != string::npos; 
+    }
+};
+
+// npos is used as functions that search for substrings or characters within a string.
+
