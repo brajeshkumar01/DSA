4377667a03f51afa5630caec4cfcd64b7de7b86c
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-03-30 11:32:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-        unordered_map<char, int> evenCount1, evenCount2;
-        unordered_map<char, int> oddCount1, oddCount2;
+       
+          if(s1.length() != s2.length()) return false;
 
-        for (int i = 0; i < s1.length(); i++) {
-            if (i % 2 == 0) { 
-                evenCount1[s1[i]]++;
-                evenCount2[s2[i]]++;
-            } else { 
-                oddCount1[s1[i]]++;
-                oddCount2[s2[i]]++;
+          unordered_map<char , int> evencount1 , evencount2;
+          unordered_map<char , int> oddcount1 , oddcount2;
+
+          for(int i = 0; i<s1.length(); i++){
+
+                 if(i % 2 == 0){
+
+                     evencount1[s1[i]]++;
+                     evencount2[s2[i]]++;
+                 }else{
+
+                     oddcount1[s1[i]]++;
+                     oddcount2[s2[i]]++;
+                 }
+          }
+            if(evencount1 != evencount2){
+
+                    return false;
             }
-        }
+              if(oddcount1 != oddcount2){
 
-       
-        if (evenCount1 != evenCount2) {
-            return false;
-        }
-
-        
-        if (oddCount1 != oddCount2) {
-            return false;
-        }
-
-       
-        return true;
+                   return false;
+              }
+                 return true;
     }
 };
