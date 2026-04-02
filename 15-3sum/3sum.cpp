42bd34ac9d422c4c854c95bd5f73bfb8f8b41e91
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-02 21:51:30 +0530

    Time: 1288 ms (5.01%) | Memory: 291.8 MB (7.17%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -9,28 +9,24 @@ public:
           sort(nums.begin() , nums.end());
           for(int i = 0; i<n-2; i++){
 
+               int left = i+1, right = n-1;
+               while(left < right){
 
-            int left =i+1, right = n-1;
+                    int sum = nums[i]+nums[left]+nums[right];
+                    if(sum == 0){
 
-            while(left < right){
+                           st.insert({nums[i], nums[left], nums[right]});
+                           left++;
+                           right--;
+                    }else if(sum < 0){
 
-                  int sum = nums[i]+nums[left]+nums[right];
-
-                  if(sum == 0){
-
-                       st.insert({nums[i], nums[left], nums[right]});
-                       left++;
-                       right--;
-                  }else if(sum < 0){
-
-                         left++;
-                  }else{
-
-                      right--;
-                  }
-            }
+                          left++;
+                    }else{
+                           right--;
+                    }
+               }
           }
-             result.assign(st.begin() , st.end());
-             return result;
+              result.assign(st.begin() , st.end());
+              return result;
     }
 };
