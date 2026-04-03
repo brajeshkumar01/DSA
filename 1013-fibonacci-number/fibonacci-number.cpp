6645669f31e1b63166a1262a55aa452c718a14c8
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-04 00:17:21 +0530

    Time: 14 ms (23.14%) | Memory: 7.8 MB (64.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int fib(int n) {
         
-          if(n == 0 || n == 1) return n;
+          if(n == 0 || n == 1){
+
+               return n;
+          }
 
           int result = fib(n-2)+fib(n-1);
-
           return result;
     }
 };
