769d67ea0f802a036889ae85f8257f196fec00bf
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-05 00:25:03 +0530

    Time: 3 ms (38.19%) | Memory: 102.8 MB (9.23%) - LeetSync

--- 4003-longest-fibonacci-subarray/longest-fibonacci-subarray.cpp
+++ 4003-longest-fibonacci-subarray/longest-fibonacci-subarray.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
 
-        int n = nums.size();
-        if(n <= 2) return 2;
-        int currlen = 2;
-        int maxlen =  2;
+          int n = nums.size();
+          if(n <= 2) return 2;
+          int currlen = 2;
+          int maxlen = 2;
 
-        for(int i = 2; i<n; i++){
+          for(int i = 2; i<n; i++){
 
-              if(nums[i] == nums[i-2]+nums[i-1]){
+                 if(nums[i] == nums[i-2]+nums[i-1]){
 
-                  currlen++;
-              }else{
+                      currlen++;
+                 }else{
 
-                   currlen = 2;
-              }
-                 maxlen = max(maxlen, currlen);
-        }
-           return maxlen;
+
+                     currlen = 2;
+                 }
+                   maxlen =max(maxlen , currlen);
+          }
+             return maxlen;
     }
 };
