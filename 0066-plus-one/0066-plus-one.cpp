3bc053892faf4b7d5b598bd58f53277e261a7423
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-06 09:26:46 +0530

    Time: 0 ms (100%), Space: 11.7 MB (27.05%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,19 +3,16 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         
          int n = digits.size();
-         //opposite iterate
-
          for(int i = n-1; i>=0; i--){
 
                if(digits[i] < 9){
 
-                    digits[i]++;
-                    return digits;
+                     digits[i]++;
+                     return digits;
                }
                   digits[i] = 0;
-         } 
-
-           digits.insert(digits.begin() , 1);
-           return digits;
+         }
+             digits.insert(digits.begin() , 1);
+             return digits;
     }
 };
