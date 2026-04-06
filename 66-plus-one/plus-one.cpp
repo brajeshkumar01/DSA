3336d93a70ccd466f3fa250bff0928980a91bb3f
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-06 09:26:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (27.05%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -5,15 +5,14 @@ public:
          int n = digits.size();
          for(int i = n-1; i>=0; i--){
 
-                if(digits[i]< 9){
+               if(digits[i] < 9){
 
-                      digits[i]++;
-                      return digits;
-                }
-
-                digits[i] = 0;
+                     digits[i]++;
+                     return digits;
+               }
+                  digits[i] = 0;
          }
-            digits.insert(digits.begin(), 1);
-            return digits;
+             digits.insert(digits.begin() , 1);
+             return digits;
     }
 };
