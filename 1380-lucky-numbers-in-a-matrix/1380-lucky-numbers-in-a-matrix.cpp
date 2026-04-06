cc968bc62535dfa6e285dd892f9ea6bcb82d1052
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-06 16:31:21 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.63%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -2,45 +2,41 @@ class Solution {
 public:
     vector<int> luckyNumbers(vector<vector<int>>& matrix) {
         
-          int n = matrix.size(), m = matrix[0].size();
+          int n = matrix.size();
+          int m = matrix[0].size();
+          vector<int>rowmin;
 
-          vector<int> rowmin;
-
-          for(int  i = 0; i<n; i++){
+          for(int i = 0; i<n; i++){
 
                int rmin = INT_MAX;
 
-               for(int j = 0; j<m; j++){
+                 for(int j = 0; j<m; j++){
 
-                     rmin = min(rmin , matrix[i][j]);
-               }
-                  rowmin.push_back(rmin);
-          }
+                       rmin = min(rmin , matrix[i][j]);
+                 }
 
-            vector<int>colmax;
+                   rowmin.push_back(rmin);
+          } 
+             vector<int>colmax;
+             for(int i = 0; i<m; i++){
+                   int cmax = INT_MIN;
+                   for(int j = 0; j<n; j++){
 
-            for(int i = 0; i<m; i++){
-
-                  int cmax = INT_MIN;
-                    for(int j = 0; j<n; j++){
-
-                          cmax = max(cmax , matrix[j][i]);
-                    }
-                      colmax.push_back(cmax);
-            }
-
-              vector<int>luckeynumber;
-
-              for(int i = 0; i<n; i++){
-
-                   for(int j = 0; j<m; j++){
-
-                     if(matrix[i][j] == rowmin[i] && matrix[i][j] == colmax[j]){
-
-                          luckeynumber.push_back(matrix[i][j]);
-                     }
+                       cmax = max(cmax , matrix[j][i]); 
                    }
-              }
-                 return luckeynumber;
+                     colmax.push_back(cmax);
+             }
+               vector<int>luckeynum;
+               for(int i = 0; i<n; i++){
+
+                  for(int j = 0; j<m; j++){
+
+                      if(matrix[i][j] == rowmin[i] && matrix[i][j] == colmax[j]){
+                             
+                             luckeynum.push_back(matrix[i][j]);
+                      } 
+                  }
+               }
+                  return luckeynum;
     }
 };
