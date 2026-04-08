8d33d75ca2225cbe149f5ac2a6ae20c148ce5e28
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-08 14:03:13 +0530

    Time: 3 ms (10.08%) | Memory: 10 MB (8.26%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool isValid(string s) {
         
-          stack<char>stack;
+          stack<int>stack;
           for(char ch : s){
 
                if(ch == '(' || ch == '[' || ch == '{'){
+                 stack.push(ch);
 
-                    stack.push(ch);
                }else{
 
                      if(stack.empty()){
@@ -21,9 +21,9 @@ public:
                           (ch == '}' && top != '{')||
                           (ch == ']' && top != '[')){
 
-                               return false;
+                              return false;
                           }
-               }
+                  }
           }
              return stack.empty();
     }
