289b16a4eaa552bc36d778b0ba59a2eddb33bcd3
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-10 11:41:28 +0530

    Time: 3 ms (71.85%), Space: 28.8 MB (79.62%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        int mindiff = INT_MAX;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k = j+1; k < n; k++){
+                    
+                    if(nums[i] == nums[j] && nums[j] == nums[k]){
+                        int dist = abs(i-j) + abs(j-k) + abs(k-i);
+                        mindiff = min(mindiff, dist);
+                    }
+                }
+            }
+        }
+
+        return (mindiff == INT_MAX) ? -1 : mindiff;
+    }
+};
