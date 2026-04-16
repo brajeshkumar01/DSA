4c65d406fb1c5a5aecb4a2d0a8b13ae5a7b398a1
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-16 14:15:49 +0530

    Time: 147 ms (98.21%), Space: 289.6 MB (65.02%) - LeetHub

--- 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.cpp
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.cpp
@@ -4,25 +4,25 @@ public:
              
           int n = nums.size();
           unordered_map<int , int>freq;
+
           for(int i = 0; i<n; i++){
 
                ++freq[nums[i]];
-          } 
+          }    
 
-            unordered_map<int , int>freqmap;
-            for(const auto& pair : freq){
+              unordered_map<int , int>freqmap;
+              for(const auto& pair : freq){
 
-                  ++freqmap[pair.second];
-            }   
+                    ++freqmap[pair.second];
+              } 
 
-               for(int num : nums){
+                for(int num : nums){
 
-                   if(freqmap[freq[num]] == 1){
+                       if(freqmap[freq[num]] == 1){
 
-                        return num;
-                   }
-               }   
-
-                 return -1;          
+                                 return num;
+                       }
+                }
+                   return -1;
     }
 };
