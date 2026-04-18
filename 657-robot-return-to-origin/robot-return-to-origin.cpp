2af40b40404abd06c1172cf72a1838e92d6cfc83
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-18 15:33:00 +0530

    Time: 3 ms (44.53%) | Memory: 10.4 MB (45.78%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool judgeCircle(string moves) {
         
-          int x = 0 , y = 0;
+          int X = 0 , y = 0;
           for(char ch : moves){
 
-            if(ch == 'U') y++;
-            else if(ch == 'D')y--;
-            else if(ch == 'L') x++;
-            else if(ch == 'R') x--;
+                  if(ch == 'U') y++;
+                  else if(ch == 'D') y--;
+                  else if(ch == 'L') X++;
+                  else if(ch == 'R') X--;
           }
-             return x == 0 && y == 0;
+             return X == 0 && y == 0;
     }
 };
