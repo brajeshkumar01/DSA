a64d8852afee229c8cb40bb95acb0486f7dc86a0
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-18 22:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (26.98%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -7,12 +7,12 @@ public:
 
               if(digits[i] < 9){
 
-                    digits[i]++;
-                    return digits;
+                   digits[i]++;
+                   return digits;
               }
                  digits[i] = 0;
         }
-          digits.insert(digits.begin() , 1);
-          return digits;
+              digits.insert(digits.begin() , 1);
+              return digits;
     }
 };
