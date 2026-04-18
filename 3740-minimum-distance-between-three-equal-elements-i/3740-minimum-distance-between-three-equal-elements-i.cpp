ca8522770b092211da90b57fbe1d7754069f4124
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-18 16:14:19 +0530

    Time: 8 ms (11.13%), Space: 28.8 MB (64.56%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -5,20 +5,21 @@ public:
          int n = nums.size();
          int mindiff = INT_MAX;
 
-          for(int i = 0; i<n; i++){
+         for(int i = 0; i<n; i++){
 
-              for(int j = i+1; j<n; j++){
+               for(int j = i+1; j<n; j++){
 
-                   for(int k = j+1; k < n; k++){
+                    for(int k = j+1; k<n; k++){
 
-                       if(nums[i] == nums[j] && nums[j] == nums[k]){
+                        if(nums[i] == nums[j] && nums[j] == nums[k]){
 
-                           int dist = abs(i-j)+abs(j-k)+abs(k-i);
-                           mindiff = min(mindiff , dist);
-                       }
-                   }
-              }
-          }
+                                int dist = abs(i-j) + abs(j-k) + abs(k-i);
+                                mindiff = min(mindiff , dist);
+                        }  
+                    }
+               }
+         }
+
             return (mindiff == INT_MAX) ? -1 : mindiff;
     }
 };
