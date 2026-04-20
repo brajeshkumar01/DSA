de856ad1a8435de0b3504d73e5d0c23deec2f513
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-20 15:14:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -4,18 +4,17 @@ public:
         
            int n = colors.size();
            int diff = 0;
+            
+              for(int i = 0; i<n; i++){
 
-           for(int i = 0; i<n; i++){
+                    for(int j = n-1; j>=0; j--){
 
-               for(int j = n-1; j>=0; j--){
+                           if(colors[i] != colors[j]){
 
-                    if(colors[i] != colors[j]){
-
-                         diff = max(diff , abs(j - i));
-     
+                                 diff = max(diff , abs(j - i));
+                           }
                     }
-               }
-           }
-              return diff;
+              }
+                 return diff;
     }
 };
