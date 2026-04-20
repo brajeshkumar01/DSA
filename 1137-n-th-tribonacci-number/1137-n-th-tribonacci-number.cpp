96ddc5acfc000b17130dff91f6deb8d0b475ad07
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-20 18:31:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (65.92%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,16 +4,15 @@ public:
         
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-
-        int a = 0 , b = 1 , c = 1;
+        int a = 0 , b = 1, c = 1;
         int next = 0;
+
         for(int i = 2; i<n; i++){
-             next = a + b + c;
-             
-             a = b;
-             b = c;
-             c = next;
+                next = a + b + c;
+                a = b;
+                b = c;
+                c = next;
         }
-           return c;
+           return next;
     }
 };
