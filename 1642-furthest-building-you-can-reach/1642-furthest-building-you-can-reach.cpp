652e5fb02ea54959ae18061a47da333195dab936
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-24 23:00:25 +0530

    Time: 11 ms (65.4%), Space: 58.2 MB (58.24%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -7,20 +7,21 @@ public:
            for(int i = 0; i<n-1; i++){
 
                  int diff = heights[i+1] - heights[i];
+
                  if(diff > 0){
 
                       pq.push(diff);
                  }
                    if(pq.size() > ladders){
 
-                        bricks -= pq.top();
-                        pq.pop();
+                          bricks -= pq.top();
+                          pq.pop();
                    }
                      if(bricks < 0){
 
                           return i;
                      }
            }
-              return n-1;  // yadi bricks 0 nahi hua then we can complete all the building
+              return n-1;
     }
 };
