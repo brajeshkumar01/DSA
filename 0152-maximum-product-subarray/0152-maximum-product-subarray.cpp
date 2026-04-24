6227f6c08d065ef124bb35de17c5c3f11aa6190e
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-24 13:28:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -7,17 +7,18 @@ public:
         int minprod = nums[0];
         int result  = nums[0];
 
-          for(int i = 1; i<n; i++){
+           for(int i = 1; i<n; i++){
 
-               if(nums[i] < 0){
+                if(nums[i] < 0){
 
-                    swap(maxprod, minprod);
-               }
-                 maxprod = max(nums[i] , nums[i]*maxprod);
-                 minprod = min(nums[i] , nums[i]*minprod);
+                      swap(maxprod , minprod);
+                }
+                   maxprod = max(nums[i] , nums[i]*maxprod);
+                   minprod = min(nums[i] , nums[i]*minprod);
 
-                 result = max(result , maxprod);
-          }
-             return result;
+                   result = max(result , maxprod);
+           }
+                 
+                 return result;
     }
 };
