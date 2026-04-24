4781a16bf587064aea6b44a8055e604072a9aec7
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-24 19:21:13 +0530

    Time: 15 ms (23.46%), Space: 55.2 MB (29.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,18 +4,19 @@ public:
         
           int n = nums.size();
           int res = 1;
-          sort(nums.begin(), nums.end());
 
-          for(int i = 0; i<n; i++){
+           sort(nums.begin() ,  nums.end());
 
-                if(res == nums[i]){
+           for(int i = 0; i<n; i++){
+
+                 if(res == nums[i]){
 
                       res++;
-                }else if(res < nums[i]){
+                 }else if(res < nums[i]){
 
-                       break;
-                }
-          }
-             return res;
+                      break;
+                 }
+           }
+              return res;
     }
 };
