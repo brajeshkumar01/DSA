62227c04bd0b4de3974e252b6f0db6d28314a22d
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-04-28 01:20:05 +0530

    Time: 2 ms (10.57%), Space: 8.6 MB (21.41%) - LeetHub

---
+++ 3908-valid-digit-number/3908-valid-digit-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool validDigit(int n, int x) {
+        
+
+               string str = to_string(n);
+
+               if(str[0]-'0' == x){
+
+                  return false;
+               }
+                
+                 for(char c : str){
+
+                     if(c - '0' == x){
+
+                          return true;
+                     }
+                 }
+
+                  return false;
+    }
+};
