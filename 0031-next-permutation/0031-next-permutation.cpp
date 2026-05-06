cdcdfda8607feabba0cb21e30816884f37c3a4c5
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-05-06 09:56:24 +0530

    Time: 0 ms (100%), Space: 15.5 MB (92.56%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -7,28 +7,28 @@ public:
 
         // opposite iterate
 
-           for(int i = n-2; i>=0; i--){
+          for(int i = n-2; i>=0; i--){
 
-                  if(nums[i] < nums[i+1]){
+                if(nums[i] < nums[i+1]){
 
                        index = i;
                        break;
-                  }
-           }
-              if(index == -1){
-
-                    reverse(nums.begin() , nums.end());
-                    return;
-              }
-
-                for(int  i = n-1; i>= index; i--){
-
-                       if(nums[i] > nums[index]){
-
-                             swap(nums[i] , nums[index]);
-                             break;
-                       }
                 }
-                   return reverse(nums.begin()+1+index, nums.end());
+          }
+             if(index == -1){
+
+                   reverse(nums.begin(), nums.end());
+                   return;
+             }
+                for(int i = n-1; i >= index; i--){
+
+                        if(nums[i] > nums[index]){
+
+                               swap(nums[i], nums[index]);
+                               break;
+                        }
+                }
+
+                 return reverse(nums.begin()+1+index , nums.end());
     }
 };
