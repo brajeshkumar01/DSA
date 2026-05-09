94aff2b177f1ac52b39daaf7f7d4b8d0fa2cd9ce
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-05-09 09:24:48 +0530

    Time: 0 ms (100%), Space: 15.1 MB (56.53%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -7,16 +7,19 @@ public:
 
          if(originalrow * originalcol != r * c)
 
-                return mat;
-             vector<vector<int>>reshape(r , vector<int>(c));
-             for(int i= 0; i<originalrow; i++){
+            return mat; 
 
-                   for(int j = 0; j<originalcol; j++){
+           vector<vector<int>>reshape(r , vector<int>(c));
 
-                          int index = i * originalcol + j;
-                          reshape[index/c][index % c] = mat[i][j];
-                   }
-             }
-                return reshape;
+           for(int i = 0; i<originalrow; i++){
+
+               for(int j = 0; j<originalcol; j++){
+
+                    int index = i * originalcol + j;
+                    reshape[index/c][index % c] = mat[i][j];
+               }
+           } 
+
+             return reshape; 
     }
 };
