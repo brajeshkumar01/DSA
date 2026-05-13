4bfef304052c7f94ea1aaa136e32ff50c5b8788b
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-05-13 09:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.39%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -5,16 +5,15 @@ public:
          if(n == 0) return 0;
          if(n == 1 || n == 2) return 1;
 
-         int a = 0 , b = 1, c = 1;
+         int a = 0 , b = 1 , c = 1;
          int next = 0;
-            
-             for(int i = 2; i<n; i++){
-
-                    next = a+b+c;
-                    a = b;
-                    b = c;
-                    c = next;
-             }
-               return c;
+        
+         for(int i = 2; i<n; i++){
+                 next = a+b+c;
+               a = b ;
+               b = c;
+               c = next;
+         }
+            return c;
     }
 };
