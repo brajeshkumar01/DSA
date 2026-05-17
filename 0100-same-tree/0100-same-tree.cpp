db521b2ec0e30de62e709062ade202af7be7b000
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-05-17 17:48:16 +0530

    Time: 0 ms (100%), Space: 12.8 MB (46.38%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,9 +15,8 @@ public:
         
           if(!p && !q) return true;
           if(!p || !q) return false;
-          
 
-          if(p->val != q->val) return false;
+          if(p -> val != q->val) return false;
 
           return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
     }
