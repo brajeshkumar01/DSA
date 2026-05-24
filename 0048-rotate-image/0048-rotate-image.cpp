f257feae206b381b8c2bd4e5679427e5a70dd2f0
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-05-24 15:14:55 +0530

    Time: 0 ms (100%), Space: 10.3 MB (33.77%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -7,23 +7,24 @@ public:
           int m = matrix.size();
           int n = matrix[0].size();
 
-            for(int i = 0; i<m; i++){
+          for(int i = 0; i<m; i++){
 
-                  for(int j = i; j<n; j++){
+              for(int j = i; j<n; j++){
 
-                         int temp = matrix[i][j];
-                         matrix[i][j] = matrix[j][i];
-                         matrix[j][i] = temp;
+                     int temp = matrix[i][j];
+                     matrix[i][j] = matrix[j][i];
+                     matrix[j][i] = temp;
+              }
+          }
+
+             for(int i = 0; i < m; i++){
+
+                  for(int j = 0; j < n/2; j++){
+
+                        int temp = matrix[i][j];
+                        matrix[i][j] = matrix[i][n-j-1];
+                        matrix[i][n-j-1] = temp;
                   }
-            }
-               for(int i = 0; i<m; i++){
-
-                  for(int j = 0; j<n/2; j++){
-
-                       int temp = matrix[i][j];
-                       matrix[i][j] = matrix[i][n-j-1];
-                       matrix[i][n-j-1] = temp;
-                  }
-               }
+             }
     }
 };
