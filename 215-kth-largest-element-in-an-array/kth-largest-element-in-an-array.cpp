1628f7bf59e31229b87ffa8349eee02c31b960c9
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-06-07 22:20:20 +0530

    Time: 26 ms (72.22%) | Memory: 61.7 MB (19.49%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,15 +4,15 @@ public:
         
         priority_queue<int , vector<int>, greater<int>>maxheap;
 
-         for(int num : nums){
+        for(int num : nums){
 
               maxheap.push(num);
-
               if(maxheap.size() > k){
 
-                  maxheap.pop();
+                   maxheap.pop();
               }
-         }
-            return maxheap.top();
+        }
+
+          return maxheap.top();
     }
 };
