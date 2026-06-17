ca5faa8fbf5708f1ae71eef410e0d8ae3ce005fe
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-06-17 19:12:00 +0530

    Time: 1239 ms (7.33%), Space: 291.7 MB (7.65%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -9,26 +9,26 @@ public:
 
            for(int i = 0; i<n-2; i++){
 
-                 int left = i+1 , right = n-1;
+              int left = i+1, right = n-1;
+              while(left < right){
 
-                   while(left < right){
+                   int sum = nums[i]+nums[left]+nums[right];
+                   if(sum == 0){
 
-                          int sum = nums[i]+ nums[left] + nums[right];
-                          if(sum == 0){
+                       st.insert({nums[i] , nums[left] , nums[right]});
+                      left++;
+                      right--;
+                   }else if(sum < 0){
 
-                                st.insert({nums[i] , nums[left] , nums[right]});
-                                left++;
-                                right--;
-                          }else if(sum < 0){
+                         left++;
 
-                               left++;
-                          }else{
+                   }else{
 
-                                right--;
-                          }
+                        right--;
                    }
+              }
            }
-               result.assign(st.begin() , st.end());
-               return result;
+              result.assign(st.begin() , st.end());
+              return result;
     }
 };
