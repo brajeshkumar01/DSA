b96c0b6205161c5cf2d09de09da798edf9069f35
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-06-19 14:51:45 +0530

    Time: 0 ms (100%), Space: 15.1 MB (33.63%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -4,16 +4,17 @@ public:
           
           int n = nums.size();
           int leftsum = 0;
-          int rightsum = accumulate(nums.begin() , nums.end(), 0);
+          int rightsum = accumulate(nums.begin() , nums.end() , 0);
+             
+             for(int i = 0; i<n; i++){
 
-            for(int i = 0; i<n; i++){
-
-                  int x = nums[i];
+                  int x = nums[i]; 
                   rightsum -= x;
-                  nums[i] = (rightsum >= leftsum) ? rightsum - leftsum : leftsum - rightsum;
-                  leftsum += x;
-            }
 
-             return nums;
+                  nums[i] = (rightsum >= leftsum) ? rightsum - leftsum : leftsum - rightsum;
+
+                  leftsum += x;
+             }
+                return nums;
     }
 };
