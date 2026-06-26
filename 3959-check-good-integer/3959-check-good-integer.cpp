65f7a552addcf8d73da1227d71f8e8b3bc410cb9
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-06-26 23:48:57 +0530

    Time: 0 ms (100%), Space: 8.4 MB (6.87%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.cpp
+++ 3959-check-good-integer/3959-check-good-integer.cpp
@@ -3,18 +3,16 @@ public:
     bool checkGoodInteger(int n) {
           
           string str = to_string(n);
-          int m = str.size();
           int digitsum = 0;
           int squaresum = 0;
-
+          int m = str.size();
           for(int i = 0; i<m; i++){
-              
-               int digit = str[i] - '0';
-                digitsum += digit;
-                squaresum+= digit * digit;
 
+               int digit = str[i]-'0';
                 
+                digitsum += digit;
+                squaresum += digit * digit;
           }
-              return (squaresum - digitsum >= 50);
+            return (squaresum - digitsum >= 50);
     }
 };
