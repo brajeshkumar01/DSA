ea306ba7b5e308a42413911f6c7e17070227303f
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-07-01 14:20:01 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.37%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,15 +4,16 @@ public:
         
           int m = matrix.size();
           int n = matrix[0].size();
-           
-            // transpose
+
+          // create transpose of matrix
+
           for(int i = 0; i<m; i++){
 
               for(int j = i; j<n; j++){
 
-                    int temp = matrix[i][j];
-                    matrix[i][j] = matrix[j][i];
-                    matrix[j][i] = temp;
+                        int temp = matrix[i][j];
+                        matrix[i][j] = matrix[j][i];
+                        matrix[j][i] = temp;
               }
           }
              for(int i = 0; i<m; i++){
