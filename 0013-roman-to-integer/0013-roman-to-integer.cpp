235f7ce6ad5a3cb324e954894d8b0c38ff3052d1
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-07-02 09:59:04 +0530

    Time: 16 ms (5.7%), Space: 13.5 MB (17.9%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,20 +5,23 @@ public:
          int n = s.length();
          unordered_map<char , int> romanmap{
 
-            {'I' , 1}, {'V' , 5} , {'X' , 10}, {'L', 50},{'C' , 100}, {'D' , 500},{'M', 1000}
+            {'I' , 1}, {'V', 5} , {'X' , 10}, {'L' , 50}, {'C' , 100},{'D' , 500},
+            {'M' , 1000}
          };
 
            int result = 0;
+
            for(int i = 0; i<n; i++){
 
                if(i < n-1 && romanmap[s[i]] < romanmap[s[i+1]]){
 
-                    result -= romanmap[s[i]];
+                     result -= romanmap[s[i]];
                }else{
 
                      result += romanmap[s[i]];
                }
            }
-              return result;
+
+             return result;
     }
 };
