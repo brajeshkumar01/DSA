c247a3f96940eca86f29ddc854a444007397023e
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-07-07 21:59:07 +0530

    Time: 0 ms (100%), Space: 12.1 MB (14.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -5,13 +5,13 @@ public:
          sort(strs.begin() , strs.end());
          int n = strs.size();
          string first = strs[0];
-         string last  = strs[n-1];
+         string last = strs[n-1];
 
          int i = 0;
 
-         while(i < first.size() && i < last.size() && first[i] == last[i]){
+         while(i < first.length() && i < last.length() && first[i] == last[i]){
 
-             i++;
+               i++;
          }
            return first.substr(0 , i);
     }
