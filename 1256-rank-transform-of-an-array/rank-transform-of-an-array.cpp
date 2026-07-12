681ae4ee607b0aa0cf2434de13e7a7bab1e52720
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-07-12 12:10:41 +0530

    Time: 86 ms (7.53%) | Memory: 52.3 MB (5.09%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-       int n = arr.size();
-       if(n == 0) return {};
+      
+         int n = arr.size();
+         if(n == 0) return {};
 
-       set<int>uniqueele(arr.begin() , arr.end());
-       unordered_map<int , int> ranks;
-       int rank = 1;
+         set<int>uniquele(arr.begin() , arr.end());
+         unordered_map<int , int> ranks;
+         int rank = 1;
 
-       for(const int &ele : uniqueele)
+         for(const int &ele : uniquele)
+                 
+                 ranks[ele] = rank++;
+                 vector<int>ans;
 
-              ranks[ele] = rank++;
+                 for(int &ele : arr)
 
-              vector<int>ans;
-              for(int &ele : arr)
+                    ans.push_back(ranks[ele]);
 
-                   ans.push_back(ranks[ele]);
+                    return ans;
        
-          return ans;
     }
 };
