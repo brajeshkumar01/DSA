51c15c1a80a28bf4bfe01b4f5db683eace6a7fbd
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-07-20 12:36:43 +0530

    Time: 6 ms (6.35%) | Memory: 8.6 MB (21.06%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -3,12 +3,12 @@ public:
     int reverse(int x) {
         
          long long ans = 0;
+
          while(x != 0){
 
-              ans = ans * 10+ x % 10;
+              ans = ans * 10 + x % 10;
               x /= 10;
          }
-
-           return (ans > INT_MAX || ans < INT_MIN) ? 0 : ans;
+           return(ans > INT_MAX || ans < INT_MIN) ? 0 : ans;
     }
 };
