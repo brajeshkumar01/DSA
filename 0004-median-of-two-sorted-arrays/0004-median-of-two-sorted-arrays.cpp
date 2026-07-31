a0a32382fc04d7612acdac80165e6b814a36b5f0
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-07-31 07:58:44 +0530

    Time: 4 ms (18.95%), Space: 96 MB (9.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,38 +4,41 @@ public:
         
          int n1 = nums1.size();
          int n2 = nums2.size();
+
          vector<int>merged;
-         int i = 0 , j = 0;
+         int i = 0, j = 0;
 
          while(i < n1 && j < n2){
 
-               if(nums1[i] < nums2[j]){
+              if(nums1[i] < nums2[j]){
 
                     merged.push_back(nums1[i]);
                     i++;
-               }else{
-
-                   merged.push_back(nums2[j]);
-                   j++;
-               }
-         } 
-            while(i < n1){
-
-                   merged.push_back(nums1[i]);
-                   i++;
-            }
-              while(j < n2){
+              }else{
 
                     merged.push_back(nums2[j]);
                     j++;
               }
-                int n = n1+n2;
-                if(n % 2 == 0){
+         }
+            while(i < n1){
 
-                       return (merged[n/2-1] + merged[n/2])/2.0;
-                }else{
+                 merged.push_back(nums1[i]);
+                 i++;
+            }
 
-                      return(merged[n/2]);
-                }
+             while(j < n2){
+
+                 merged.push_back(nums2[j]);
+                 j++;
+             }
+
+              int n = n1 + n2;
+              if(n % 2 == 0){
+
+                  return (merged[n/2-1] + merged[n/2])/2.0;
+              }else{
+
+                   return (merged[n/2]);
+              }
     }
 };
