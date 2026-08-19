ebb2115e4b35818bf123cf3d0375d1cb3c65ee97
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-08-19 18:37:22 +0530

    Time: 0 ms (100%), Space: 55.5 MB (20.22%) - LeetHub

---
+++ 4024-nearest-available-drone/4024-nearest-available-drone.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int nearestDrone(vector<vector<int>>& drones, vector<int>& target) {
+        
+        int n = drones.size();
+        int mindistance = INT_MAX;
+        int ans = -1;
+
+        for(int i = 0; i<n; i++){
+
+                int distance = abs(drones[i][0] - target[0]) + abs(drones[i][1] - target[1]);
+               
+                if(distance <= drones[i][2]){
+
+                       if(distance < mindistance){
+
+                            mindistance = distance;
+                            ans = i;
+                       }
+                }
+                
+        }
+           return ans;
+    }
+};
