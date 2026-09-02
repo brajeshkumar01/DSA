d9f3ed58c9022d99af1a2806aab8c0fc01cd3d5f
Brajesh kumar <140653010+brajeshkumar01@users.noreply.github.com>
2026-09-02 10:21:23 +0530

    Time: 4 ms (30.55%) | Memory: 10.7 MB (17.50%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,18 +3,18 @@ public:
     bool isPalindrome(int x) {
         
          string str = to_string(x);
-         int n = str.length();
-         int left = 0,right = n-1;
+         int n = str.size();
+         int left = 0, right = n-1;
 
          while(left < right){
 
                if(str[left] != str[right]){
 
-                     return false;
+                            return false;
                }
                   left++;
                   right--;
          }
-             return true;
+            return true;
     }
 };
